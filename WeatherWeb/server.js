20754d61a84903b3502d1d4d3b7f5eaf869b69f4
Gusts Kaksis <gusts.kaksis@gmail.com>
2018-06-27 09:00:07 +0300

    Implemented a simple web view

--- WeatherWeb/server.js
+++ WeatherWeb/server.js
@@ -11,42 +11,47 @@ var weather = require('./services/weather.js');
 
 var config = {};
 if (fs.existsSync('config.json')) {
-  var configStr = fs.readFileSync('config.json');
-  try {
-    config = JSON.parse(configStr);
-  } catch (e) {
-    console.log('Failed to parse a config file', e.message)
-    process.exit(0);
-  }
+    var configStr = fs.readFileSync('config.json');
+    try {
+        config = JSON.parse(configStr);
+    } catch (e) {
+        console.log('Failed to parse a config file', e.message)
+        process.exit(0);
+    }
 } else {
-  console.log('No config.json found');
-  process.exit(0);
+    console.log('No config.json found');
+    process.exit(0);
 }
 
 // Start HTTP server
 
+console.log("Start server on: " + config['http_port']);
 server.listen(config['http_port']);
 
 // Connect database
 
+console.log("Connect to: " + config['psql_url']);
 const client = new pg.Client(config['psql_url']);
 client.connect(function(err)
 {
-  if (err)
-  {
-      done();
-      console.log(err);
-      return;
-  }
-  startServices(client);
+    if (err)
+    {
+        done();
+        console.log(err);
+        return;
+    }
+    startServices(client);
 });
 
 // Start services
 
 function startServices(db)
 {
-  weather.start(config, db);
-  
-  app.get('/', weather.view);
+    console.log("Starting services");
+
+    weather.start(config, db);
+
+    app.use(express.static('public'))
+    app.get('/', weather.view);
 }
 
--- WeatherWeb/services/weather.js
+++ WeatherWeb/services/weather.js
@@ -12,13 +12,60 @@ function startService(cfg, db)
     database = db;
     config = cfg;
 }
-function finishService()
+function stopService()
 {
     
 }
 function viewService(req, res)
 {
-    
+    database.query(
+        "SELECT measure_date, pressure, latitude, longitude, altitude " +
+        "FROM weather_data " +
+        "ORDER BY measure_date DESC " +
+        "LIMIT 10",
+        [],
+        function(err, result)
+        {
+            if (err)
+            {
+                console.log("Failed to read weather data: ", err);
+                return;
+            }
+
+            var table = '';
+            for (var a in result.rows)
+            {
+                table += '<tr><td>' + result.rows[a]['measure_date'] + '</td>';
+                table += '<td>' + result.rows[a]['latitude'] + '</td>';
+                table += '<td>' + result.rows[a]['longitude'] + '</td>';
+                table += '<td>' + result.rows[a]['altitude'] + '</td>';
+                table += '<td>' + result.rows[a]['pressure'] + '</td></tr>';
+            }
+
+            res.send(
+                '<!DOCTYPE html>' +
+                '<html lang="en">' +
+                '<head>' +
+                    '<title>Weather Near You</title>' +
+                    '<meta charset="utf-8">' +
+                    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">' +
+                    '<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+                    '<meta name="viewport" content="width=device-width, initial-scale=1">' +
+                    '<link rel="stylesheet" href="/assets/style.css">' +
+                    '<script data-cfasync="false" src="/assets/scripts.js"></script>' +
+                '</head>' +
+                '<body>' +
+                    '<section class="content">' +
+                        '<table>' +
+                            '<thead><tr><th>Time</th><th>Latitude</th><th>Longitude</th><th>Altitude</th><th>Pressure</th></tr></thead>' +
+                            '<tbody>' + table + '</tbody>' +
+                        '</table>' +
+                    '</section>' +
+                '</body>' +
+                '</html>'
+            );
+        }
+    );
 }
 
 module.exports = {
